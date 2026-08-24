ce7b36f14e4ba3fd0d7b65261633d9e1a752830e
zakiabuzarqa <zakizarqa789@gmail.com>
2026-08-24 21:46:51 +0300

    packages parseed

--- src/main/kotlin/Main.kt
+++ src/main/kotlin/Main.kt
@@ -1,5 +1,18 @@
 package org.izaki
 
-fun main() {
+import org.izaki.dataholder.PackagePriority
+import org.izaki.dataholder.PackageRaw
+import org.izaki.parsers.loadCsvFile
+import org.izaki.parsers.parsePackages
+import java.io.File
 
+fun main() {
+    val packagesCsvLines = loadCsvFile("src/main/resources/packages.csv")
+    val fleetCsvLines = loadCsvFile("src/main/resources/fleet.csv")
+    val routesCsvLines = loadCsvFile("src/main/resources/routes.csv")
+    val warehousesCsvLines = loadCsvFile("src/main/resources/warehouses.csv")
+
+    val packageRawList = parsePackages(packagesCsvLines)
+
+    println(packageRawList)
 }
