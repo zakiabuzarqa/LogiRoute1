94a0034d40126fd81c13b82d859fac0365cf0cf0
zakiabuzarqa <zakizarqa789@gmail.com>
2026-08-24 19:49:48 +0300

    happy initial commit!

---
+++ build.gradle.kts
@@ -0,0 +1,22 @@
+plugins {
+    kotlin("jvm") version "2.4.0"
+}
+
+group = "org.izaki"
+version = "1.0-SNAPSHOT"
+
+repositories {
+    mavenCentral()
+}
+
+dependencies {
+    testImplementation(kotlin("test"))
+}
+
+kotlin {
+    jvmToolchain(25)
+}
+
+tasks.test {
+    useJUnitPlatform()
+}
---
+++ src/main/kotlin/Main.kt
@@ -0,0 +1,5 @@
+package org.izaki
+
+fun main() {
+
+}
